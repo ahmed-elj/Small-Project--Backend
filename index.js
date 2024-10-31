f1a9d02d0e11003f7846ddefee60b7376240207d
fsigo <eljahmed40@gmail.com>
2024-10-31 19:16:24 +0100

    added encryption

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/user'); // Import the User model
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secretKey = 'your_secret_key'; // Replace with your actual secret key
+
 
 const app = express();
 const port = 3000;
@@ -24,12 +28,37 @@ app.post('/users', async (req, res) => {
         });
         await user.save();
         res.status(201).send(user);
+        console.log(`user's info sent! ${user.name}`)
     } catch (error) {
         res.status(400).send(error.message);
     }
 });
 
+
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).send('Invalid email or password.');
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(400).send('Invalid email or password.');
+        }
+
+        const token = jwt.sign({ _id: user._id }, secretKey, { expiresIn: '1h' });
+        res.send({ message: 'Login successful', token });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -20,6 +21,15 @@ const userSchema = new mongoose.Schema({
         minlength: 5,
         maxlength: 1024
     }
+}, {versionKey:false});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
