5d75f6389e48cef7eff8f13f486608d825974c73
fsigo <eljahmed40@gmail.com>
2024-11-02 16:17:27 +0100

    Fix syntax, update signup response message

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,24 +1,24 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const secretKey = 'your-secret-key-here';
-const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const secretKey = "your-secret-key-here";
+const User = require("../models/user");
 
 //singup function
 exports.singup = async (req, res) => {
-    try {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
-        await user.save();
-        res.status(201).send(user);
-        console.log(`user's info sent! ${user.name}`);
-        res.send(message="user singup!");
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await user.save();
+    console.log(`user's info sent! ${user.name}`);
+    res.status(201).send({ message: "singup successful" });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
+
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
